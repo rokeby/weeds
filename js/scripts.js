2af4f5b75dbf0a5d84107cadc03de58943ff4a60
rokeby <gary.zhexi.zhang@gmail.com>
2021-08-05 21:43:16 +0100

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// var i = 0
+
+// for (let i= 0; i < 100; i++) {
+	
+// }
+
+setInterval(Moveon, 100)
+
+function Moveon() {
+	$(".text").prepend("&nbsp; ")
+	console.log("hello")
+}
+
+// console.log(i)
