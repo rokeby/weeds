2994634b23a848fa5c0dc3d950536c2434a699e2
rokeby <gary.zhexi.zhang@gmail.com>
2021-08-06 01:39:06 +0100

    basic layout

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,35 @@
-// var i = 0
+// setInterval(Moveon, 100)
 
-// for (let i= 0; i < 100; i++) {
-	
+// function Moveon() {
+// 	$(".text").prepend("&nbsp; ")
+// 	console.log("hello")
 // }
 
-setInterval(Moveon, 100)
+console.log("hi")
 
-function Moveon() {
-	$(".text").prepend("&nbsp; ")
-	console.log("hello")
+scrollVert();
+var scrollLeft = 0;
+
+function scrollVert() {
+  $('html, body, *').off('mousewheel').mousewheel(function(e, delta) {
+    this.scrollTop -= (delta * 40);
+    console.log(this.scrollTop)
+    e.preventDefault();
+    setTimeout(function() {
+      if ($(window).scrollTop() + $(window).height() == $(document).height())
+        scrollHoriz();
+    }, 0)
+
+  });
 }
-
-// console.log(i)
+function scrollHoriz() {
+  $('html, body, *').off('mousewheel').mousewheel(function(e, delta) {
+    this.scrollLeft -= (delta * 40);
+    // console.log(delta)
+    e.preventDefault();
+    scrollLeft=this.scrollLeft
+    setTimeout(function() {
+      if (scrollLeft == 0) scrollVert();
+    }, 0)
+  });
+}
