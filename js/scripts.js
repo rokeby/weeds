f09cf0312d72b4cd83b4f76c7f502e4b6c5f29e2
rokeby <gary.zhexi.zhang@gmail.com>
2021-08-06 14:26:20 +0100

    scroll control jquery

--- js/scripts.js
+++ js/scripts.js
@@ -5,31 +5,67 @@
 // 	console.log("hello")
 // }
 
-console.log("hi")
-
 scrollVert();
 var scrollLeft = 0;
+var containerHeight = $('.essay-segment').height()
+var containerWidth = $('.essay-segment').width()
 
 function scrollVert() {
-  $('html, body, *').off('mousewheel').mousewheel(function(e, delta) {
-    this.scrollTop -= (delta * 40);
-    console.log(this.scrollTop)
-    e.preventDefault();
+  $('.essay-english').off('mousewheel').mousewheel(function(e, delta) {
+    this.scrollTop -= (delta * 1);
+    e.preventDefault(); 
+    var sum = document.getElementsByClassName('essay-english')[0].scrollHeight - document.getElementsByClassName('essay-english')[0].scrollTop
     setTimeout(function() {
-      if ($(window).scrollTop() + $(window).height() == $(document).height())
+    	// console.log(sum)
+      if (sum == containerHeight ) {
         scrollHoriz();
+        scrollVertStop()
+		}
     }, 0)
+  });
+}
 
-  });
+function scrollVertStop() {
+  $('.essay-english').off('mousewheel').mousewheel(function(e, delta) {
+    this.scrollTop -= (delta * 0);
+    e.preventDefault(); 
+   })
 }
+
+function scrollHorizStop() {
+  $('.essay-container').off('mousewheel').mousewheel(function(e, delta) {
+    this.scrollTop -= (delta * 0);
+    e.preventDefault(); 
+   })
+}
+
+
 function scrollHoriz() {
-  $('html, body, *').off('mousewheel').mousewheel(function(e, delta) {
-    this.scrollLeft -= (delta * 40);
-    // console.log(delta)
+  $('.essay-container').off('mousewheel').mousewheel(function(e, delta) {
+    this.scrollLeft -= (delta * 1);
     e.preventDefault();
-    scrollLeft=this.scrollLeft
+    scrollLeft = this.scrollLeft
     setTimeout(function() {
-      if (scrollLeft == 0) scrollVert();
+      if (scrollLeft == containerWidth ) {
+      	scrollVert();
+        scrollHorizStop();
+        } else if (scrollLeft == 0) {
+        	scrollVert();
+        	scrollHorizStop();
+        }
     }, 0)
   });
 }
+
+// function scrollVertChinese() {
+//   $('.essay-chinese').off('mousewheel').mousewheel(function(e, delta) {
+//     this.scrollTop -= (delta * 1);
+//     e.preventDefault(); 
+//     var sum = document.getElementById('.essay-chinese').scrollHeight - document.getElementById('.essay-chinese').scrollTop
+//     setTimeout(function() {
+//       if (sum == containerHeight ) {
+//         scrollHoriz();
+// 		}
+//     }, 0)
+//   });
+// }
