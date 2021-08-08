e5024ef0167e8558f64fe069b6dc78a7ea48ec62
rokeby <gary.zhexi.zhang@gmail.com>
2021-08-08 17:00:10 +0100

    essay inject:

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,38 @@
 $(document).ready(function(){
 
-    var text = essayObj.en
-    console.log(text)
+    var textEN = essayObjEN.en
+    var textCN = essayObjCN.cn
+    var essayMod = "<div class='essay-container'><div class='essay-segment'><div class='essay-english' lang='en'></div><div class='essay-chinese' lang='cn'></div></div></div>"
 
-    for(let i = 1; i < Object.keys(text).length; i++) {
-    	for (let j = 1; j < text[i].length; j++) { 
-			$(".intro-text").prepend("<p>" + text[i][j] + "<p>")
-		}
+
+    for(let i = 0; i < Object.keys(textEN).length; i++) {
+
+    	$( essayMod ).clone().appendTo(".scroll-container").attr("id", i+1)
+
+    	for (let j = 0; j < $(".essay-english").length; j++) {
+
+	    	for (let k = 0; k < textEN[i+1].length; k++) { 
+
+	    		var essayPara = textEN[i+1][k]
+				$("#" + (i+1) + " .essay-english" ).append("<p>" + essayPara + "</p>")
+			}
+    	}
     }
-    
+
+
+    for(let i = 0; i < Object.keys(textCN).length; i++) {
+
+    	// $( essayMod ).clone().appendTo(".scroll-container")
+
+    	for (let j = 0; j < $(".essay-chinese").length; j++) {
+
+	    	for (let k = 0; k < textCN[i+1].length; k++) { 
+
+	    		var essayPara = textCN[i+1][k]
+				$("#" + (i+1) + " .essay-chinese" ).append("<p>" + essayPara + "</p>")
+			}
+    	}
+    }
+
 });
 
