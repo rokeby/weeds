62272cd1a8579953db7aff00f2a61d843022cb26
rokeby <gary.zhexi.zhang@gmail.com>
2021-08-10 00:54:36 +0100

    buttons demo

--- js/scripts.js
+++ js/scripts.js
@@ -2,37 +2,68 @@ $(document).ready(function(){
 
     var textEN = essayObjEN.en
     var textCN = essayObjCN.cn
-    var essayMod = "<div class='essay-container'><div class='essay-segment'><div class='essay-english' lang='en'></div><div class='essay-chinese' lang='cn'></div></div></div>"
+    var $splash = $(".splash")   
+    var $essayMod = $(["<div class='essay-container'>",
+    						"<div class='essay-segment'>",
+    							"<div class='essay-english' lang='en'>",
+								"</div>",
+								"<div class='essay-chinese' lang='cn'>",
+    							"</div>",
+							"</div>",
+    					"</div>"].join("\n")
+					)
+
+console.log(textEN[1].length)
+
+	$(".scenes, .right-arrow").click( function() {
+		$(".scroll-container").html($essayMod)
+
+		for(let i = 0; i < textEN[1].length; i++) {
+			$(".essay-english").append("<p>" + textEN[1][i] + "</p>").fadeIn()
+			$(".essay-chinese").append("<p>" + textCN[1][i] + "</p>").fadeIn()
+		}
+	})
+
+	$(".left-arrow").click( function() {
+		$(".scroll-container").html($splash)
+	})
+
+    // console.log(essayMod)
 
 
-    for(let i = 0; i < Object.keys(textEN).length; i++) {
-
-    	$( essayMod ).clone().appendTo(".scroll-container").attr("id", i+1)
-
-    	for (let j = 0; j < $(".essay-english").length; j++) {
-
-	    	for (let k = 0; k < textEN[i+1].length; k++) { 
-
-	    		var essayPara = textEN[i+1][k]
-				$("#" + (i+1) + " .essay-english" ).append("<p>" + essayPara + "</p>")
-			}
-    	}
-    }
+    // $( essayMod ).appendTo(".scroll-container")
 
 
-    for(let i = 0; i < Object.keys(textCN).length; i++) {
+//     for(let i = 0; i < Object.keys(textEN).length; i++) {
 
-    	// $( essayMod ).clone().appendTo(".scroll-container")
+//     	$( essayMod ).clone().appendTo(".scroll-container").attr("id", i+1)
 
-    	for (let j = 0; j < $(".essay-chinese").length; j++) {
+//     	for (let j = 0; j < $(".essay-english").length; j++) {
 
-	    	for (let k = 0; k < textCN[i+1].length; k++) { 
+// 	    	for (let k = 0; k < textEN[i+1].length; k++) { 
 
-	    		var essayPara = textCN[i+1][k]
-				$("#" + (i+1) + " .essay-chinese" ).append("<p>" + essayPara + "</p>")
-			}
-    	}
-    }
+// 	    		var essayPara = textEN[i+1][k]
+// 				$("#" + (i+1) + " .essay-english" ).append("<p>" + essayPara + "</p>")
+// 			}
+//     	}
+//     }
+
+//     for(let i = 0; i < Object.keys(textCN).length; i++) {
+
+//     	// $( essayMod ).clone().appendTo(".scroll-container")
+
+//     	for (let j = 0; j < $(".essay-chinese").length; j++) {
+
+// 	    	for (let k = 0; k < textCN[i+1].length; k++) { 
+
+// 	    		var essayPara = textCN[i+1][k]
+// 				$("#" + (i+1) + " .essay-chinese" ).append("<p>" + essayPara + "</p>")
+// 			}
+//     	}
+//     }
+
 
 });
+
+
 
