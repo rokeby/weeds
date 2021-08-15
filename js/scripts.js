cc8aecb1c213d7c6f9e4bb2f61e469f3a193a212
rokeby <gary.zhexi.zhang@gmail.com>
2021-08-15 13:08:56 +0100

    implemented intro page

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,16 @@ $(document).ready(function(){
 
     const essay = essayObj
     const $splash = $(".splash")
-    const $essayModule = $(["<div class='essay-container'>",
+    const $introElements = $(["<div class='splash'>",
+							"<img src='./assets/main-image.png'/>",
+							"<div class='intro'>",
+								"<div class='intro-text'>",
+								"</div>",
+							"</div>",
+    					"</div>"].join("\n")
+					)
+
+    const $essayElements = $(["<div class='essay-container'>",
     						"<div class='essay-segment'>",
     							"<div class='essay-english' lang='en'>",
 								"</div>",
@@ -15,6 +24,14 @@ $(document).ready(function(){
     let section = 0
     const essayLength = Object.keys(essay).length
 
+    sectionUpdate()
+
+    $(".scenes li").click( function() {
+    	section = $(this).data("attr")
+		sectionUpdate()
+		addImages()
+    })
+
 	$(".right-arrow").click( function() {
 		section += 1
 		if(section >= (essayLength - 1)) { section = 0 }
@@ -26,11 +43,7 @@ $(document).ready(function(){
 	})
 
 	$(".arrow").click( function() {
-
-		$(".scroll-container").html($essayModule)
 	
-		console.log(section)
-
 		sectionUpdate()
 		addImages()
 
@@ -45,20 +58,39 @@ $(document).ready(function(){
 
 		let $sectionEN = $("<p>").append("" + essay[section].en.title + "<br>") 
 		let $sectionCN = $("<p>").append("" + essay[section].cn.title + "<br>")
+		let $introEN = $("<p>")
+		let $introCN = $("<p>")
 
-		for(let i = 0; i < essay[section].en.text.length; i++) {
-			$sectionEN.append("<br>" + essay[section].en.text[i] + "<br>")
-			$sectionCN.append("<br>" + essay[section].cn.text[i] + "<br>")		
+		if(section == 0) {
+
+			$(".scroll-container").html($introElements)
+
+			for(let i = 0; i < essay[section].en.text.length; i++) {
+				$introEN.append(essay[section].en.text[i] + "<br>")
+				$introCN.append(essay[section].cn.text[i] + "<br>")		
+			}
+
+			$(".intro-text").html($introEN).append($introCN)
+
+    	} else if (section > 0) {
+
+			$(".scroll-container").html($essayElements)
+
+
+			for(let i = 0; i < essay[section].en.text.length; i++) {
+				$sectionEN.append("<br>" + essay[section].en.text[i] + "<br>")
+				$sectionCN.append("<br>" + essay[section].cn.text[i] + "<br>")		
+			}
+
+			$(".essay-english").html($sectionEN)
+			$(".essay-chinese").html($sectionCN)
 		}
-
-		$(".essay-english").html($sectionEN)
-		$(".essay-chinese").html($sectionCN)
 	}
 
 	function addImages() {
 
 		// turn text into array in order to randomly insert image spans.
-		const numObjects = 6;
+		const numObjects = 3;
 
 		const $obstruct = ["<span class='obstruct-left'></span>", "<span class='obstruct-right'></span>"]
 		const $target = [$(".essay-english p"), $(".essay-chinese p")]
@@ -67,11 +99,13 @@ $(document).ready(function(){
 
 			const objArr = $target[i][0].outerHTML.split(" ")
 			for (let j=0; j<numObjects; j++) {
-				objArr.splice(objArr.length/4 + (Math.floor(Math.random() * objArr.length/2)), 0, $obstruct[1])
-				objArr.splice(objArr.length/4 + (Math.floor(Math.random() * objArr.length/2)), 0, $obstruct[0])
+				objArr.splice(objArr.length/6 + (Math.floor(Math.random() * objArr.length/2)), 0, $obstruct[1])
+				objArr.splice(objArr.length/6 + (Math.floor(Math.random() * objArr.length/2)), 0, $obstruct[0])
 			}
+
 			newText = objArr.join(" ")
 			$target[i].html(newText)
+
 		}
 
 		addImageListeners()
@@ -90,5 +124,15 @@ $(document).ready(function(){
 			$(".obstruct-left").removeClass("obstruct-large")
 		})
 	}
+
+	// function scrollListener() {
+	//   $('.essay-english, .essay-chinese').off('mousewheel').mousewheel(function(e, delta) {
+	//     this.scrollTop -= (delta * 1);
+	//     e.preventDefault(); 
+	//     console.log(scrollTop)
+	//     // var sum = document.getElementsByClassName('essay-english')[0].scrollHeight - document.getElementsByClassName('essay-english')[0].scrollTop
+	//   });
+	// }
+
 
 });
